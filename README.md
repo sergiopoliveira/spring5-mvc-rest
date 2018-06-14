c37677a04408f925c4172e087ad6a6e9cd0250b6
sergiopoliveira <sergiopoliveira@outlook.com>
2018-06-14 11:30:39 +0100

    Spring5 REST

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![codecov](https://codecov.io/gh/sergiopoliveira/spring5-mvc-rest/branch/master/graph/badge.svg)](https://codecov.io/gh/sergiopoliveira/spring5-mvc-rest)
+
 # Spring Framework 5 MVC Rest Application
---
+++ src/main/java/com/sergio/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.sergio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sergio.domain.Category;
+
+/**
+ * Created by jt on 9/24/17.
+ */
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
